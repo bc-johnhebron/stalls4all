2ca2416627d4a378146eea0e269d244c826c40cb
Chelsea Shaw <chelseas@spiceworks.com>
2017-06-25 01:08:18 -0500

    [CS] Add star directive to ng-app, add ng-views folder to public, move nav to shared folder, 3-column layout on list view

--- public/js/ng-app.js
+++ public/js/ng-app.js
@@ -1,29 +1,32 @@
-var app = angular.module('stalls4all', ['dataService','mainCtrl']);
-
+var app = angular.module('stalls4all', ['mainCtrl', 'locationsService'], function($interpolateProvider) {
+  $interpolateProvider.startSymbol('<#');
+  $interpolateProvider.endSymbol('#>');
+});
 
 var ctrl = angular.module('mainCtrl', []);
-
-ctrl.controller('mainController', ['$scope', 'dataService', function($scope, dataService){
-  // object to hold all the data for the new comment form
-    $scope.locations = {};
-
+ctrl.controller('mainController', ['$scope','Locations', function($scope, Locations){
+    $scope.data = {};
+    $scope.userLoc = false;
     // loading variable to show the spinning loading icon
     $scope.loading = true;
 
-    // get all the comments first and bind it to the $scope.comments object
-    // use the function we created in our service
-    // GET ALL COMMENTS ==============
+    // GET ALL LOCATIONS ==============
     Locations.getAll()
-        .success(function(data) {
-            $scope.locations = data;
-            $scope.loading = false;
-        });
+      .success(function(data) {
+        $scope.data = data;
+        $scope.loading = false;
+      });
+
+    $scope.getUserLoc = function(){
+      // TODO: Get user location with geolocation service
+      $scope.userLoc = !$scope.userLoc;
+    }
+
 }]);
 
-var data = angular.module('dataService', []);
-
+var data = angular.module('locationsService', []);
 data.factory('Locations', function($http) {
-
+    console.log('Locations factory');
     return {
         // get all the comments
         getAll : function() {
@@ -35,9 +38,58 @@ data.factory('Locations', function($http) {
         getSinglePhotos : function(id) {
           return $http.get('/api/locations/'+id+'/photos');
         },
-        getReviews : function(locID,reviewID) {
-          return $http.get('/api/locations'+locID+'/reviews/'+reviewID);
+        getReviews : function(locID) {
+          return $http.get('/api/locations/'+locID+'/reviews/');
+        },
+        getReview : function(locID,reviewID) {
+          return $http.get('/api/locations/'+locID+'/reviews/'+reviewID);
         }
     }
+});
 
+app.directive('starRating', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      numStars: '=rating'
+    },
+    templateUrl: 'ng-views/star-rating.html'
+  };
+});
+
+app.directive('reviews', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      locID: '=locID'
+    },
+    controller: function ($scope,Locations) {
+      $scope.reviews;
+      Locations.getReviews(4)
+        .success(function(data) {
+          $scope.reviews = data;
+        });
+
+      $scope.getReviews = function () {
+          //Call external scope's function
+          var name = 'New Customer Added by Directive';
+          $scope.add();
+
+          //Add new customer to directive scope
+          $scope.customers.push({
+              name: name
+          });
+      };
+    },
+    templateUrl: 'ng-views/reviews.html'
+  };
+});
+
+app.filter('range', function() {
+  return function(val, range) {
+    range = parseInt(range);
+    for (var i=0; i<range; i++)
+      val.push(i);
+    return val;
+  };
 });
