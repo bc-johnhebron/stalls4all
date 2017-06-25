528321e2022f6d4522e25f701abe4861b9f3a6fc
John Hebron <john.hebron@bigcommerce.com>
2017-06-25 14:14:28 -0500

    changed stylesheet to https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="//stalls4all.herokuapp.com/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div id="app">
