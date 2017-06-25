6c86b5dd0a5f6fb9b9cac4ec3ff8ee09ce658578
Chelsea Shaw <chelseas@spiceworks.com>
2017-06-24 23:34:46 -0500

    [CS] Styling, add ng-app to public, views/index.php, and ng.blade.php for angular integration

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/list', function() {
+    return View::make('index'); 
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
